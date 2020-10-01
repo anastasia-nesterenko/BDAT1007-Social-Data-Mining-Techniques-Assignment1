eeba51e4dfdc3722e742121fc7f54776f19f08fd
Anastasia <anastasia.nesterenko.dev@gmail.com>
2020-10-01 16:56:58 -0400

    POST for game (Crud)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from firebase import firebase
-from flask import Flask, json
+from flask import Flask, json, request, url_for
 from flask import render_template
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 firebaseApp = firebase.FirebaseApplication('https://letzteleben-398ef.firebaseio.com/', None)
@@ -16,19 +17,16 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/games')
-def get_games():
-    content = firebaseApp.get('/games', None)
-    array = []
-    if content is not None:
-        temp_serialized = json.dumps(content)
-        temp_json = json.loads(temp_serialized)
-        for item in temp_json:
-            array.append(temp_json[item])
-
-        return render_template('games.html', content=array)
-    else:
-        return render_template('games.html')
+@app.route('/games', methods=['GET', 'POST'])
+def games():
+    if request.method == 'GET':
+        return render_template('games.html', content=firebase_get_games())
+    elif request.method == 'POST':
+        if request.form['add_game'] == 'Add':
+            # TODO Add game to firebase
+            return render_template('games.html', content=firebase_get_games())
+        else:
+            return {'id': 10000}, 200
 
 
 @app.route('/games/<int:game_id>')
@@ -38,6 +36,17 @@ def get_game_by_id(game_id):
         return content
     else:
         return "No data about this game"
+
+
+def firebase_get_games():
+    content = firebaseApp.get('/games', None)
+    array = []
+    if content is not None:
+        temp_serialized = json.dumps(content)
+        temp_json = json.loads(temp_serialized)
+        for item in temp_json:
+            array.append(temp_json[item])
+    return array
 
 
 if __name__ == "__main__":
